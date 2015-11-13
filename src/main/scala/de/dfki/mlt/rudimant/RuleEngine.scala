547e79d859bf8bf06849bd5f7a2cafdde8885a95
Miroslav Janíček <miroslav.janicek@dfki.de>
2015-11-13 16:25:58 +0100

    Adding simple implementations for adding and listing rules in the engine.

--- src/main/scala/de/dfki/mlt/rudimant/RuleEngine.scala
+++ src/main/scala/de/dfki/mlt/rudimant/RuleEngine.scala
@@ -2,17 +2,24 @@ package de.dfki.mlt.rudimant
 
 import de.dfki.mlt.rudimant.dsl.Rule
 
+import scala.collection.mutable.ArrayBuffer
+
 class RuleEngine {
 
   val log = Log(this.getClass)
 
+  private val _rules = ArrayBuffer.empty[RuleSet]
+
   def add(rs: RuleSet): Unit = {
-    log.debug("Adding rule set {}", rs)
-    ???
+    log.debug("Adding ruleset {}", rs)
+    if (!_rules.contains(rs)) {
+      _rules += rs
+    }
+    else {
+      log.warning("Engine already contains ruleset {}, skipping", rs)
+    }
   }
 
-  def rules: Iterable[Rule[_]] = {
-    ???
-  }
+  def rules: Iterable[Rule[_]] = _rules flatMap { _.rules }
 
 }
