4331a6888c8143fb169645c4355adc4cdb04c81d
Anna Welker <anna.welker@dfki.de>
2016-03-05 11:52:52 +0100

    started to add proper environments; problems in visitor not fixed yet

---
+++ src/main/java/Versuch2/Environment.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Versuch2;
+
+import Versuch2.abstractTree.AbstractType;
+import java.util.HashMap;
+
+/**
+ *
+ * @author anna
+ */
+public class Environment {
+  
+  private HashMap<String, AbstractType> memory;
+  private int depth;
+  
+  public Environment(int depth){
+    this.depth = depth;
+    this.memory = new HashMap<String, AbstractType>();
+  }
+  
+  public boolean isVisibleFrom(int somewhere){
+    return this.depth < somewhere;
+  }
+  
+  public int getDepth(){
+    return this.depth;
+  }
+  
+  public void put(String v, AbstractType t){
+    this.memory.put(v, t);
+  }
+  
+  public boolean containsKey(String k){
+    return this.memory.containsKey(k);
+  }
+  
+  public AbstractType get(String k){
+    return this.memory.get(k);
+  }
+  
+}
--- src/main/java/Versuch2/Mem.java
+++ src/main/java/Versuch2/Mem.java
@@ -5,15 +5,66 @@
  */
 package Versuch2;
 
-import Versuch1.RobotContext;
 import Versuch2.abstractTree.AbstractType;
-import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Stack;
 
 /**
  *
  * @author anna
  */
 public class Mem {
+
+  private Stack<Environment> environment;
+  private int depthAtm;
+
+  public Mem() {
+    this.environment = new Stack<Environment>();
+    this.depthAtm = 0;
+  }
   
-  public static HashMap<String, AbstractType> memory = new HashMap<String, AbstractType>();
+  public int getActualDepth(){
+    return this.depthAtm;
+  }
+
+  public void addElement(String variable, AbstractType type) {
+    this.environment.peek().put(variable, type);
+  }
+
+  public boolean existsVariable(String variable) {
+    return this.getVariableType(variable) != null;
+  }
+
+  public AbstractType getVariableType(String variable) {
+    Environment actual = this.environment.peek();
+    if (!actual.containsKey(variable)) {
+      int position = actual.getDepth();
+      Iterator it = this.environment.listIterator();
+      while (it.hasNext()) {
+        if (!actual.containsKey(variable)) {
+          actual = (Environment) it.next();
+          while (!actual.isVisibleFrom(position) && it.hasNext()) {
+            actual = (Environment) it.next();
+          }
+        }
+      }
+      return actual.get(variable);
+    }
+    return null;
+  }
+
+  public void addNextEnvironment(Environment env) {
+    this.depthAtm = env.getDepth();
+    this.environment.add(env);
+  }
+
+  public Environment getNextEnvironment() {
+    this.depthAtm = this.environment.peek().getDepth();
+    return this.environment.peek();
+  }
+
+  public void leaveEnvironment() {
+    this.environment.pop();
+    this.depthAtm = this.environment.peek().getDepth();
+  }
 }
