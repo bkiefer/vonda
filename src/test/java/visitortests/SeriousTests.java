6b2106c118ea877199e7042fc2671520dc984fc5
Anna Welker <anna.welker@dfki.de>
2016-09-07 10:38:56 +0200

    Test added

---
+++ src/test/java/visitortests/SeriousTests.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package visitortests;
+
+import de.dfki.mlt.rudi.GrammarMain;
+import java.io.File;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Anna Welker, anna.welker@dfki.de
+ */
+public class SeriousTests {
+
+  public SeriousTests() {
+  }
+
+  @BeforeClass
+  public static void setUpClass() {
+    File outDir = new File("target/test/testfiles");
+    if(!outDir.exists()){
+      outDir.mkdirs();
+    }
+  }
+
+  @AfterClass
+  public static void tearDownClass() {
+  }
+
+  @Before
+  public void setUp() {
+  }
+
+  @After
+  public void tearDown() {
+  }
+
+//  @Test
+//  public void TestGreetUser() throws Exception {
+//    String[] strings = new String[]{"src/test/resources/serious/greet_user.rudi",
+//      "target/test/testfiles", "-nt"};
+//    GrammarMain.main(strings);
+//  }
+
+  @Test
+  public void FirstRuleTest() throws Exception {
+    String[] strings = new String[]{"src/test/resources/FirstRule.rudi",
+      "target/test/testfiles", "-nt"};
+    GrammarMain.main(strings);
+  }
+}
