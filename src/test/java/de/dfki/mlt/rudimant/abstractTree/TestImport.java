6ef382440130f7ae4c797e0d1edf7cf173f5982a
Sophie Henning <sophie_elisabeth.henning@dfki.de>
2017-03-07 10:27:22 +0100

    Commit needed to merge, TestImport doesn't work yet

---
+++ src/test/java/de/dfki/mlt/rudimant/abstractTree/TestImport.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.dfki.mlt.rudimant.abstractTree;
+
+import static org.junit.Assert.*;
+import static de.dfki.mlt.rudimant.abstractTree.TstUtils.*;
+
+import org.junit.*;
+
+/**
+ *
+ * @author sophie
+ */
+public class TestImport {
+  
+  @BeforeClass
+  public static void setUpClass() {
+    setUpEmpty();
+  }
+  
+//  //generates the empty string
+//  @Test
+//  public void test(){
+//    String in = "import basic.MaxTest1;";
+//    String r = getGeneration(in);
+//    String exp = "import basic.MaxTest1;";
+//    assertEquals(exp, r);
+//  }
+//  
+//  //throws a FileNotFoundException
+//  @Test
+//  public void test2(){
+//    String in = "import MaxTest1;";
+//    String r = getGeneration(in);
+//    String exp = "import MaxTest1;";   
+//  }
+  
+}
