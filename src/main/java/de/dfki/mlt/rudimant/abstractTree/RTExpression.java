30af912be5e904673a7742a1af1f02d10f2dab21
Bernd Kiefer <bernd.kiefer@dfki.de>
2017-05-12 15:53:11 +0200

    Clean up ensureboolean

--- src/main/java/de/dfki/mlt/rudimant/abstractTree/RTExpression.java
+++ src/main/java/de/dfki/mlt/rudimant/abstractTree/RTExpression.java
@@ -5,18 +5,10 @@
  */
 package de.dfki.mlt.rudimant.abstractTree;
 
-import static de.dfki.mlt.rudimant.Constants.*;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import de.dfki.mlt.rudimant.Constants;
 import de.dfki.mlt.rudimant.Mem;
-import de.dfki.mlt.rudimant.agent.DialogueAct;
 
 /**
  * a special kind of the RudiTree is an expression; expressions can have types
@@ -66,106 +58,74 @@ public abstract class RTExpression extends RudiTree {
 
   public String getInnerType() { return Mem.getInnerType(type); }
 
-  public RTExpression ensureBoolean() {
-    ExpBoolean result = null;
-    USingleValue right = null;
+  public RTExpression fixFields(RTExpression b) {
+    b.positions = positions;
+    b.fullexp = fullexp;
+    return b;
+  }
 
+  public RTExpression ensureBooleanBasic() {
     if (this instanceof ExpBoolean) {
       return this;
-    } else if ("boolean".equals(type)){
+    }
+
+    if ("boolean".equals(type)){
       if(this instanceof USingleValue){
         return this;
       } else {
         // if this is a funccall with type boolean, we'd still like to have it
         // as a boolean, at least wrapped up; if it is a fieldaccess kind of funccall, we have
-    	// to go on testing for each part if it is null
-        result = new ExpBoolean(this, null, null);
-        if(!(this instanceof UFieldAccess)){
-          result.positions = positions;
-          result.fullexp = fullexp;
-          return result;
-        }
+        // to go on testing for each part if it is null
+        return fixFields(new ExpBoolean(this, null, null));
       }
     }
+
     // this is some other kind of expression, turn it into a comparison or
     // method call returning a boolean
     // TODO check if there's something missing for RDF types
     if (isStringOrComplexType()) {
-      /* TODO: MAKE THIS WORK PROPERLY. If it's a function call, it must
-       * be treated differently
-      USingleValue newSingle = new USingleValue("null", "Object");
-      ExpBoolean newLeft =
-          new ExpBoolean(fullexp, this, newSingle , "!=");
-      ExpBoolean newRight =
-          new ExpBoolean(fullexp, this, null, ".isEmpty() == false");
-      result = new ExpBoolean(fullexp, newLeft, newRight, "&&");
-      int lastpos = positions[positions.length - 1];
-      newSingle.positions = newLeft.positions = newRight.positions
-          = new int[]{ lastpos, lastpos };
-      */
-      result = new ExpBoolean(this, null, "exists(");
-    } else if (result == null){
-      if (type == null) {
-        right = new USingleValue("null", "Object");
+      return fixFields(new ExpBoolean(this, null, "exists("));
+    }
+
+    RTExpression result = null;
+    USingleValue right = null;
+
+    if (type == null) {
+      right = new USingleValue("null", "Object");
+      right.positions = positions;
+      right.fullexp = fullexp;
+      result = fixFields(new ExpBoolean(this, right, "!="));
+    } else {
+      String cleanType = Mem.convertXsdType(type);
+      if (! type.equals(cleanType)) {
+        result = fixFields(new ExpBoolean(this, null, "exists("));
+      } else {
+        switch (type) {
+        case "int":
+        case "float":
+        case "double":
+          right = new USingleValue("0", type);
+          break;
+        default:
+          right = new USingleValue("null", "Object");
+          break;
+        }
         right.positions = positions;
         right.fullexp = fullexp;
-        result = new ExpBoolean(this, right, "!=");
-	  } else { 
-	    String cleanType = Mem.convertXsdType(type);
-	    if (! type.equals(cleanType)) {
-	      result = new ExpBoolean(this, null, "exists(");
-	    } else {
-	      switch (type) {
-	      case "int":
-	      case "float":
-	      case "double":
-	        right = new USingleValue("0", type);
-	        break;
-	      default:
-	        right = new USingleValue("null", "Object");
-	        break;
-	      }
-	      right.positions = positions;
-	      right.fullexp = fullexp;
-	      result = new ExpBoolean(this, right, "!=");
-	    }
-	  }
+        result = fixFields(new ExpBoolean(this, right, "!="));
+      }
     }
-    if(this instanceof UFieldAccess){
-        int s = ((UFieldAccess)this).parts.size();
-        if(((UFieldAccess)this).parts.get(s - 2).type != null && 
-      		  ((UFieldAccess)this).parts.get(s - 2).type.equals(Constants.DIALOGUE_ACT_TYPE)){
-        	USingleValue property = new USingleValue("\"" + 
-		    		  	((UFieldAccess)this).parts.get(s - 1).fullexp + "\"", "String");
-    	    property.positions = positions;
-      	    property.fullexp = fullexp;
-    	    result = new ExpBoolean(((UFieldAccess)this).parts.get(s - 2), property, "hasSlot(");
-        }
-        result.positions = positions;
-        result.fullexp = fullexp;
-        if(s > 2){
-  	      // to avoid NullPointerexceptions, test on null on all levels
-  	      List<RTExpression> smaller = new ArrayList<>();
-  	      smaller.addAll(((UFieldAccess)this).parts);
-  	      smaller.remove(s - 1);
-  	      List<String> smallerRep = new ArrayList<>();
-  	      smallerRep.addAll(((UFieldAccess)this).representation);
-  	      smallerRep.remove(s - 1);
-  	      UFieldAccess partAccess = new UFieldAccess(smaller, smallerRep);
-  	      partAccess.positions = positions;
-  	      partAccess.fullexp = fullexp;
-  	      result = new ExpBoolean(partAccess.ensureBoolean(), result, "&&");
-        } else {
-          RTExpression first = ((UFieldAccess)this).parts.get(0);
-  	      first.positions = positions;
-  	      first.fullexp = fullexp;
-  	      result = new ExpBoolean(first.ensureBoolean(), result, "&&");
-        }
-    }
-    result.positions = positions;
-    result.fullexp = fullexp;
     return result;
   }
+
+  public RTExpression ensureBoolean() {
+    if (this instanceof UFieldAccess) {
+      UFieldAccess ufa = (UFieldAccess)this;
+      return ufa.ensureBooleanUFA();
+    }
+    return ensureBooleanBasic();
+  }
+
 
   public abstract void propagateType(String upperType);
 
--- src/main/java/de/dfki/mlt/rudimant/abstractTree/UFieldAccess.java
+++ src/main/java/de/dfki/mlt/rudimant/abstractTree/UFieldAccess.java
@@ -5,7 +5,10 @@
  */
 package de.dfki.mlt.rudimant.abstractTree;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import de.dfki.mlt.rudimant.Constants;
 
 /**
  * this represents an access to the ontology (will result in an rdf object in
@@ -45,4 +48,28 @@ public class UFieldAccess extends RTExpLeaf {
   }
 
   public Iterable<? extends RudiTree> getDtrs() { return parts; }
+
+  public RTExpression ensureBooleanUFA() {
+    int s = parts.size() - 1;
+    if (s == 0) {
+      return parts.get(s).ensureBooleanBasic();
+    }
+
+    List<RTExpression> smaller = parts.subList(0, s);
+    List<String> smallerRep = representation.subList(0, s);
+    UFieldAccess first = new UFieldAccess(smaller, smallerRep);
+    first.positions = positions;
+    first.fullexp = fullexp;
+    RTExpression right;
+    if (Constants.DIALOGUE_ACT_TYPE.equals(parts.get(s - 1).type)) {
+      USingleValue property = new USingleValue("\"" +
+          parts.get(s).fullexp + "\"", "String");
+      property.positions = positions;
+      property.fullexp = fullexp;
+      right = fixFields(new ExpBoolean(first, property, "hasSlot("));
+    } else {
+      right = this.ensureBooleanBasic();
+    }
+    return fixFields(new ExpBoolean(first.ensureBooleanUFA(), right, "&&"));
+  }
 }
