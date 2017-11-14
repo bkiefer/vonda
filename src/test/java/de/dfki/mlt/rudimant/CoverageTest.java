7d42e1deec9ca0846cca755469597ee855fc7710
Bernd Kiefer <bernd.kiefer@dfki.de>
2017-11-14 19:26:53 +0100

    Add test to check coverage of AllYouCanDo.rudi

---
+++ src/test/java/de/dfki/mlt/rudimant/CoverageTest.java
@@ -0,0 +1,24 @@
+package de.dfki.mlt.rudimant;
+import static de.dfki.mlt.rudimant.compiler.tree.TstUtils.RESOURCE_DIR;
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import de.dfki.mlt.rudimant.compiler.CompilerMain;
+
+public class CoverageTest {
+
+  @Test
+  public void Test() throws Exception {
+    // enter here the file whose compilation you'd like to debug
+    CompilerMain.main(new String[]{
+      "-o", "target/generated/",// "-v",
+      "-r", RESOURCE_DIR + "ontos/pal.quads.ini",
+      "doc/AllYouCanDo.rudi",
+    });
+
+    assertTrue(new File("target/generated/AllYouCanDo.java").exists());
+  }
+}
