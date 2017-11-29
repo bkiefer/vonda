3df13ffa7623a9296c9b081f475cd024609c7866
Christophe Biwer <christophe.biwer@dfki.de>
2017-11-29 14:46:13 +0100

    fixed missing refactoring

--- src/main/java/de/dfki/mlt/rudimant/common/DebugClient.java
+++ src/main/java/de/dfki/mlt/rudimant/common/DebugClient.java
@@ -6,49 +6,54 @@ import org.apache.xmlrpc.XmlRpcException;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * an XML-RPC Client to connect to the debugger in Java
  */
 public class DebugClient implements LogPrinter {
 
-	/**
-	 * define the server URL plus port number
-	 */
-	private static String SERVER_URL = "http://localhost:";
-	//private static String SERVER_URL = "http://localhost:1408";
+  /**
+   * define the server URL plus port number
+   */
+  private static String SERVER_URL = "http://localhost:";
+  //private static String SERVER_URL = "http://localhost:1408";
 
-	XmlRpcClient client;
+  private static Logger logger = LoggerFactory.getLogger(DebugServer.class);
 
-	/**
-	 * call with, e.g.,
-	 *   java -cp .:../lib/* de/dfki/lt/hfc/server/HfcClient 'select distinct ?p where ?s ?p ?o'
-	 * note the quotes (' ... ') to make the query a single string
-	 * alternatively, one might further supply the server URL as a second argiment, e.g.,
-	 *   java -cp .:./lib/* de/dfki/lt/hfc/server/HfcClient 'select distinct ?p where ?s ?p ?o ?b ?e' http://localhost:1408
-	 * @throws MalformedURLException
-	 */
-	public DebugClient (int port) {
-	  XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
-	  try {
-	    config.setServerURL(new URL(DebugClient.SERVER_URL));
-	  } catch (MalformedURLException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	  }
-	  client = new XmlRpcClient();
-	  client.setConfig(config);
-	}
+  XmlRpcClient client;
 
+  /**
+   * call with, e.g., java -cp .:../lib/* de/dfki/lt/hfc/server/HfcClient
+   * 'select distinct ?p where ?s ?p ?o' note the quotes (' ... ') to make the
+   * query a single string alternatively, one might further supply the server
+   * URL as a second argiment, e.g., java -cp .:./lib/*
+   * de/dfki/lt/hfc/server/HfcClient 'select distinct ?p where ?s ?p ?o ?b ?e'
+   * http://localhost:1408
+   *
+   * @throws MalformedURLException
+   */
+  public DebugClient(int port) {
+    XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+    try {
+      config.setServerURL(new URL(DebugClient.SERVER_URL));
+    } catch (MalformedURLException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    client = new XmlRpcClient();
+    client.setConfig(config);
+  }
 
-	@Override
-	public void printLog(RuleInfo ruleId, boolean[] result) {
-	  Object[] params = { ruleId, result };
-	  try {
-	    client.execute("Agent.printLog", params);
-	  } catch (XmlRpcException e) {
-	    logger.error(e.getMessage());
-	  }
-	}
+  @Override
+  public void printLog(RuleInfo ruleId, boolean[] result) {
+    Object[] params = {ruleId, result};
+    try {
+      client.execute("Agent.printLog", params);
+    } catch (XmlRpcException e) {
+      logger.error(e.getMessage());
+    }
+  }
 
 }
--- src/main/java/de/dfki/mlt/rudimant/common/DebugServer.java
+++ src/main/java/de/dfki/mlt/rudimant/common/DebugServer.java
@@ -1,5 +1,8 @@
 package de.dfki.mlt.rudimant.common;
 
+import de.dfki.lt.hfc.WrongFormatException;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.server.PropertyHandlerMapping;
 import org.apache.xmlrpc.server.XmlRpcServer;
@@ -7,19 +10,10 @@ import org.apache.xmlrpc.webserver.WebServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import de.dfki.lt.hfc.ForwardChainer;
-import de.dfki.lt.hfc.Query;
-import de.dfki.lt.hfc.WrongFormatException;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-
 /**
  * establishes an XML-RPC server for querying information from the repository;
  * starting the server means to call the main method with exactly four arguments
+ *
  * @see main()
  *
  * @author Hans-Ulrich Krieger
@@ -29,93 +23,91 @@ public class DebugServer {
 
   private static Logger logger = LoggerFactory.getLogger(DebugServer.class);
 
-	/**
-	 * the port number for the web server
-	 */
-	private int port;
+  /**
+   * the port number for the web server
+   */
+  private int port;
 
-	/**
-	 * the web server that embodies the XML-RPC server
-	 */
-	private WebServer webServer;
+  /**
+   * the web server that embodies the XML-RPC server
+   */
+  private WebServer webServer;
 
-	/**
-	 * called by the main method and is given EXACTLY four arguments:
-	 *   + port number
-	 *   + path to namespace directory
-	 *   + path to tuple directory
-	 *   + path to rule directory
-	 *
-	 * NOTE: subdirectories, i.e., recursive embeddings of files are NOT allowed
-	 * @throws IOException
-	 * @throws WrongFormatException
-	 * @throws FileNotFoundException
-	 *
-	 * @see main()
-	 */
-	private DebugServer(int port) throws IOException {
-		this.port = port;
-	}
+  /**
+   * called by the main method and is given EXACTLY four arguments: + port
+   * number + path to namespace directory + path to tuple directory + path to
+   * rule directory
+   *
+   * NOTE: subdirectories, i.e., recursive embeddings of files are NOT allowed
+   *
+   * @throws IOException
+   * @throws WrongFormatException
+   * @throws FileNotFoundException
+   *
+   * @see main()
+   */
+  private DebugServer(int port) throws IOException {
+    this.port = port;
+  }
 
-	/**
-	 * returns true iff the file does NOT start with a '.' and/or ends with a '~';
-	 * otherwise false is returned
-	 */
-	private boolean isProperFile(String name) {
-		if (name.charAt(0) == '.')
-			return false;
-		if (name.charAt(name.length() - 1) == '~')
-			return false;
-		return true;
-	}
+  /**
+   * returns true iff the file does NOT start with a '.' and/or ends with a '~';
+   * otherwise false is returned
+   */
+  private boolean isProperFile(String name) {
+    if (name.charAt(0) == '.') {
+      return false;
+    }
+    if (name.charAt(name.length() - 1) == '~') {
+      return false;
+    }
+    return true;
+  }
 
-	/**
-	 * starts the server and assigns instance fields hfc and query to the static
-	 * class fields HFC and QUERY in class HfcServerApi
-	 * @see HfcServerApi.stopServer()
-	 */
-	public synchronized void startServer() {
-		try {
-			this.webServer = new WebServer(this.port);
-			XmlRpcServer xmlRpcServer = this.webServer.getXmlRpcServer();
-			PropertyHandlerMapping phm = new PropertyHandlerMapping();
-			// HFC redirects the request to the static Query instance
-			phm.addHandler("Agent", DebuggerAgentApi.class);
-			xmlRpcServer.setHandlerMapping(phm);
-			this.webServer.start();
-			logger.info("\n Agent server started, waiting for input ...");
-		}
-		catch (XmlRpcException exception) {
-			System.err.println("\n  HfcServer: XML-RPC Fault #" +
-												 Integer.toString(exception.code) +
-												 ": " + exception.toString());
-		}
-		catch (Exception exception) {
-			System.err.println("\n  HfcServer: " + exception.toString());
-		}
-	}
+  /**
+   * starts the server and assigns instance fields hfc and query to the static
+   * class fields HFC and QUERY in class HfcServerApi
+   *
+   * @see HfcServerApi.stopServer()
+   */
+  public synchronized void startServer() {
+    try {
+      this.webServer = new WebServer(this.port);
+      XmlRpcServer xmlRpcServer = this.webServer.getXmlRpcServer();
+      PropertyHandlerMapping phm = new PropertyHandlerMapping();
+      // HFC redirects the request to the static Query instance
+      phm.addHandler("Agent", DebugAgentApi.class);
+      xmlRpcServer.setHandlerMapping(phm);
+      this.webServer.start();
+      logger.info("\n Agent server started, waiting for input ...");
+    } catch (XmlRpcException exception) {
+      System.err.println("\n  HfcServer: XML-RPC Fault #"
+        + Integer.toString(exception.code)
+        + ": " + exception.toString());
+    } catch (Exception exception) {
+      System.err.println("\n  HfcServer: " + exception.toString());
+    }
+  }
 
-	/**
-	 * the main method requires EXACTLY four (4) arguments:
-	 *   + 1st arg: port number (int)
-	 *   + 2nd arg: namespace directory (String)
-	 *   + 3rd arg: tuple directory (String)
-	 *   + 4th arg: rule directory (String)
-	 *
-	 * call with, e.g.,
-	 *   java -server -cp .:../lib/* -Xms800m -Xmx1200m de/dfki/lt/hfc/server/HfcServer 1408 ../resources/namespaces/ ../resources/tuples/ ../resources/rules/
-	 * @throws IOException
-	 * @throws WrongFormatException
-	 * @throws FileNotFoundException
-	 */
-	public static void main(String[] args) throws FileNotFoundException, WrongFormatException, IOException {
-		if (args.length != 4) {
-			System.err.println("  wrong number of arguments; required (4): port-no namespace-dir tuple-dir rule-dir");
-			System.exit(1);
-		}
-		System.out.println("\n  starting HFC Server ... ");
-		DebugServer hs = new DebugServer(Integer.parseInt(args[0]));
-		hs.startServer();
-	}
+  /**
+   * the main method requires EXACTLY four (4) arguments: + 1st arg: port number
+   * (int) + 2nd arg: namespace directory (String) + 3rd arg: tuple directory
+   * (String) + 4th arg: rule directory (String)
+   *
+   * call with, e.g., java -server -cp .:../lib/* -Xms800m -Xmx1200m de/dfki/lt/hfc/server/HfcServer 1408 ../resources/namespaces/ ../resources/tuples/ ../resources/rules/
+   *
+   * @throws IOException
+   * @throws WrongFormatException
+   * @throws FileNotFoundException
+   */
+  public static void main(String[] args) throws FileNotFoundException, WrongFormatException, IOException {
+    if (args.length != 4) {
+      System.err.println("  wrong number of arguments; required (4): port-no namespace-dir tuple-dir rule-dir");
+      System.exit(1);
+    }
+    System.out.println("\n  starting HFC Server ... ");
+    DebugServer hs = new DebugServer(Integer.parseInt(args[0]));
+    hs.startServer();
+  }
 
 }
