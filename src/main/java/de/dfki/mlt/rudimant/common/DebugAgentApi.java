a807c77547238097ac7f40027fa4f8d72b720969
Christophe Biwer <christophe.biwer@dfki.de>
2017-11-30 13:28:03 +0100

    added java logging for DebugAgentApi

--- src/main/java/de/dfki/mlt/rudimant/common/DebugAgentApi.java
+++ src/main/java/de/dfki/mlt/rudimant/common/DebugAgentApi.java
@@ -1,12 +1,16 @@
 package de.dfki.mlt.rudimant.common;
 
 import de.dfki.mlt.rudimant.agent.Agent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * the server API that is separated from the server;
  */
 public class DebugAgentApi {
   private Agent _agent;
+
+  private static Logger logger = LoggerFactory.getLogger(DebugServer.class);
 
   /**
    * Start logging a specific rule
@@ -16,5 +20,6 @@ public class DebugAgentApi {
    */
   public void setLoggingStatus(int id, int what) {
     _agent.logRule(id, what);
+    logger.debug("set logging state of rule [" + id + "] to state " + what);
   }
 }
