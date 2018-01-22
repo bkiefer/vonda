3033f182983aee60c56f2b6afebaa422b1bb039a
Christophe Biwer <christophe.biwer@dfki.de>
2018-01-22 10:49:13 +0100

    improved SimpleClient connection attempts (and its logging)

--- src/main/java/de/dfki/mlt/rudimant/common/SimpleClient.java
+++ src/main/java/de/dfki/mlt/rudimant/common/SimpleClient.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Calendar;
+import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,7 +15,7 @@ public class SimpleClient {
 
   private Socket socket;
 
-  private int _portNumber;
+  private final int _portNumber;
 
   public static String hostName = "localhost";
 
@@ -22,24 +24,52 @@ public class SimpleClient {
   /**
    * A client that connects to the server on localhost at the given port to send
    * log information to the debugger.
+   *
+   * @param portNumber
    */
   public SimpleClient(int portNumber) {
     _portNumber = portNumber;
   }
 
+  private Date nextTryToConnect;
+  private Date nextLog;
+
+  private final int noLogInterval = 10;
+  private final int reconnectInterval = 1;
+
   private boolean initClient() {
-    try {
-      socket = new Socket(hostName, _portNumber);
-      out = new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
-      logger.debug("Client has been connected.");
-      return true;
-    } catch (UnknownHostException e) {
-      logger.error(e.toString());
-      return false;
-    } catch (IOException e) {
-      logger.error(e.toString());
-      return false;
+    Date currentTime = new Date();
+    if (nextTryToConnect == null || currentTime.after(nextTryToConnect)) {
+      try {
+        /* make sure that only one try per second occurs */
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        cal.add(Calendar.SECOND, reconnectInterval);
+        nextTryToConnect = cal.getTime();
+
+        socket = new Socket(hostName, _portNumber);
+        out = new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
+        logger.debug("Client has been connected.");
+        return true;
+      } catch (UnknownHostException e) {
+        logger.error(e.toString());
+        return false;
+      } catch (IOException e) {
+        if (nextLog == null || currentTime.after(nextLog)) {
+          /* make sure only every 10 seconds this will be logged */
+          Calendar cal = Calendar.getInstance();
+          cal.setTime(new Date());
+          cal.add(Calendar.SECOND, noLogInterval);
+          nextLog = cal.getTime();
+
+          logger.error("Client could not connect (Reconnect every "
+            + reconnectInterval + " second(s), no log for " + noLogInterval
+            + " second(s)): " + e.toString());
+        }
+        return false;
+      }
     }
+    return false;
   }
 
   public boolean isConnected() {
@@ -73,11 +103,11 @@ public class SimpleClient {
     SimpleClient scl = new SimpleClient(3665);
     try {
       int i = 0;
-      while (i++ < 10) {
+      while (i++ < 50) {
         scl.send("test", "345", "12345", "14");
-        Thread.sleep(1000);
-        scl.send("printLog", Integer.toString(i), "234", "12");
-        Thread.sleep(1000);
+        Thread.sleep(250);
+//        scl.send("printLog", Integer.toString(i), "234", "12");
+//        Thread.sleep(1000);
       }
     } catch (InterruptedException e) {
       System.out.println(e);
