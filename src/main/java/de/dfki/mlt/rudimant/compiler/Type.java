a95a20f88b8e9f6d10829bc05f74c11998ee5f97
bkiefer <kiefer@dfki.de>
2018-03-08 18:01:08 +0100

    Safeguard against weird case

--- src/main/java/de/dfki/mlt/rudimant/compiler/Type.java
+++ src/main/java/de/dfki/mlt/rudimant/compiler/Type.java
@@ -519,9 +519,10 @@ public class Type {
         if (! first) sb.append(", ");
         if (pType == null)  // for visualization
           sb.append("null");
-        else if (pType.isRdfType())
-          sb.append("Object[").append(_class.toString()).append("]");
-        else
+        else if (pType.isRdfType()) {
+          String rdftype = _class == null ? "null" : _class.toString();
+          sb.append("Object[").append(rdftype).append("]");
+        } else
           pType.toDebugString(sb);
         first = false;
       }
