253db930277be523a45bf02f28aa364839a8270c
bkiefer <kiefer@dfki.de>
2018-05-23 17:44:19 +0200

    fix problem with non-termination

--- update-repo.sh
+++ update-repo.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 MAVEN_REPO="$HOME/.m2/repository"
-
-GRAFF_VERSION=0.4-SNAPSHOT
-HFC_VERSION=1.2.0-SNAPSHOT
-HFC_DB_VERSION=1.3-SNAPSHOT
-PALTECS_UTILS_VERSION=2.0-SNAPSHOT
+LOCAL_REPO=repo
+scriptdir=`dirname $0`
+cd "$scriptdir"
 
 # ARGS: groupId artefact version
 function mvi() {
-    jar_path="$MAVEN_REPO/`echo $1 | sed 's|\.|/|g'`/$2/$3/$2-$3.jar"
+    if test -z "$what" || echo " $what " | grep -q " $2 "; then
+        jar_path="$MAVEN_REPO/`echo $1 | sed 's|\.|/|g'`/$2/$3/$2-$3.jar"
+        pom_path="${jar_path%%jar}pom"
 
-    mvn install:install-file -Dfile="$jar_path" -DgroupId="$1" \
-        -DartifactId="$2" -Dversion="$3" -Dpackaging=jar \
-        -DlocalRepositoryPath=repo
+        mvn install:install-file -Dfile="$jar_path" -DgroupId="$1" \
+            -DartifactId="$2" -Dversion="$3" -Dpackaging=jar \
+            -DlocalRepositoryPath="$LOCAL_REPO" -DpomFile="$pom_path"
+    fi
 }
 
-mkdir repo
+if test "$1" = "-u" ; then
+    shift
+    foo=$LOCAL_REPO
+    LOCAL_REPO="$MAVEN_REPO"
+    MAVEN_REPO="$foo"
+fi
+what="$*"
 
-mvi de.dfki.mlt graff "$GRAFF_VERSION"
+mkdir $LOCAL_REPO 2>/dev/null
 
-mvi de.dfki.lt.hfc hfc "$HFC_VERSION"
+mvi de.dfki.mlt graff 0.4-SNAPSHOT
 
-mvi de.dfki.lt.hfc hfc-db "$HFC_DB_VERSION"
+mvi de.dfki.lt.hfc hfc 1.2.0-SNAPSHOT
 
-mvi pal palTECS-utils "$PALTECS_UTILS_VERSION"
+mvi de.dfki.lt.hfc hfc-db 1.3-SNAPSHOT
 
-TECS_VERSION=2.0.6
-mvi de.dfki.tecs libtecs "$TECS_VERSION"
+mvi pal palTECS-utils 2.0-SNAPSHOT
+
+mvi de.dfki.tecs libtecs 2.0.6
